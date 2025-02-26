8a35cc4914607a97b9575fa61c6ddee393c4b8c0
ondyke <dikeonuohajohn@gmail.com>
2025-02-26 20:19:19 +0100

    Create AppTest.java

---
+++ AppTest.java
@@ -0,0 +1,11 @@
+ickage com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+            @Test
+                public void shouldAnswerWithTrue() {
+                                assertTrue(true);
+                                    }
+}
